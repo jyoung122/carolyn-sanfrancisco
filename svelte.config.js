4c58b2b717111a08c025ce6e6008cd16974e895f
jyoung122 <jeremyayoung@hotmail.com>
2025-12-28 13:48:01 -0500

    feat: add Netlify adapter for deployment and update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import cloudFlare from '@sveltejs/adapter-cloudflare';
+import netlify from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,13 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: cloudFlare({
-			// REF https://github.com/sveltejs/kit/blob/fd6eb9b152001a537f4277a9e597aa24405a51af/documentation/docs/25-build-and-deploy/60-adapter-cloudflare.md#usage
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
+		adapter: netlify(),
 		alias: {
 			$i18n: 'src/i18n'
 		}
